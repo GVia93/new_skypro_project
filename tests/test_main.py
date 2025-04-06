4f8ba880a575b7895790bde2569b6e258b6c6ee9
gvia <gritcaev.vv@gmail.com>
2025-04-06 18:28:14 +0300

    add test

--- tests/test_main.py
+++ tests/test_main.py
@@ -173,3 +173,20 @@ def test_invalid_sum():
     grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
     with pytest.raises(TypeError):
         _ = smartphone1 + grass1
+
+def test_smartphone_addition_invalid():
+    """
+    Тестирует сложение Smartphone с другим типом вызывается TypeError.
+    """
+    smartphone1 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+    with pytest.raises(TypeError):
+        _ = smartphone1 + "Not a product"
+
+
+def test_lawngrass_addition_invalid():
+    """
+    Тестирует сложение LawnGrass с другим типом вызывается TypeError.
+    """
+    grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+    with pytest.raises(TypeError):
+        _ = grass1 + "Not a product"
